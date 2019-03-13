d6975285ba74e7f44feea7dc6543a1ebc7d68780
r3dwc <skillful3007@gmail.com>
2019-03-13 13:57:07 -0500

    Update ModelStep.vue

--- src/components/ModelStep.vue
+++ src/components/ModelStep.vue
@@ -45,8 +45,57 @@ export default {
       if (year === '2014' && make === 'ACURA') {
         modelList = ['ILX', 'MDX', 'RDX', 'RLX', 'TL', 'TSX']
       }
-
-
+      if (year === '2013' && make === 'ACURA') {
+        modelList = ['ILX', 'MDX', 'RDX', 'TL', 'TSX', 'ZDX']
+      }
+      if (year === '2012' && make === 'ACURA') {
+        modelList = ['MDX', 'RDX', 'RL', 'TSX', 'ZDX']
+      }
+      if (year === '2011' && make === 'ACURA') {
+        modelList = ['MDX', 'RDX', 'RL', 'TL', 'TSX', 'ZDX']
+      }
+      if (year === '2010' && make === 'ACURA') {
+        modelList = ['MDX', 'RDX', 'RL', 'TL', 'TSX', 'ZDX']
+      }
+      if (year === '2009' && make === 'ACURA') {
+        modelList = ['MDX', 'RDX', 'RL', 'TL', 'TSX']
+      }
+      if (year === '2008' && make === 'ACURA') {
+        modelList = ['MDX', 'RDX', 'RL', 'TL', 'TSX']
+      }
+      if (year === '2007' && make === 'ACURA') {
+        modelList = ['MDX', 'RDX', 'RL', 'TL', 'TSX']
+      }
+      if (year === '2006' && make === 'ACURA') {
+        modelList = ['3.2TL', 'MDX', 'RL', 'TL', 'TSX']
+      }
+      if (year === '2005' && make === 'ACURA') {
+        modelList = ['MDX', 'NSX', 'RL', 'RSX', 'TL', 'TSX']
+      }
+      if (year === '2004' && make === 'ACURA') {
+        modelList = ['3.5RL', 'MDX', 'NSX', 'RSX', 'TL', 'TSX']
+      }
+      if (year === '2003' && make === 'ACURA') {
+        modelList = ['3.2CL', '3.2TL', '3.5RL', 'MDX', 'NSX', 'RSX']
+      }
+      if (year === '2002' && make === 'ACURA') {
+        modelList = ['3.2CL', '3.2TL', '3.5RL', 'MDX', 'NSX', 'RSX']
+      }
+      if (year === '2001' && make === 'ACURA') {
+        modelList = ['3.2CL', '3.2TL', '3.5RL', 'INTEGRA', 'MDX', 'NSX']
+      }
+      if (year === '2000' && make === 'ACURA') {
+        modelList = ['3.2TL', '3.5RL', 'INTEGRA',  'NSX']
+      }
+      if (year === '1999' && make === 'ACURA') {
+        modelList = ['2.3CL', '3.0CL', '3.2TL',  '3.5RL', 'INTEGRA', 'NSX', 'SLX']
+      }
+      if (year === '1998' && make === 'ACURA') {
+        modelList = ['2.3CL', '2.5TL', '3.0CL',  '3.2TL', '3.5RL', 'INTEGRA', 'NSX', 'SLX']
+      }
+      if (year === '1997' && make === 'ACURA') {
+        modelList = ['2.5TL',  '3.2TL', '3.5RL', 'INTEGRA', 'NSX', 'SLX']
+      }
 
       // BMW Models//
       if (year === '2019' && make === 'BMW') {
